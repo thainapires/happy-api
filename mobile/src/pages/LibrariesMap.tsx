0ef1e3b0b2bd68fb6e05deaa8d2542a2470b9723
Thainá Pires <thainaspiress@gmail.com>
2020-10-16 19:50:43 -0300

    Finishing version 1

--- mobile/src/pages/LibrariesMap.tsx
+++ mobile/src/pages/LibrariesMap.tsx
@@ -1,51 +1,78 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Dimensions} from 'react-native';
 import MapView, {Marker, Callout, PROVIDER_GOOGLE} from 'react-native-maps';
 import {Feather} from '@expo/vector-icons';
 import mapMarker from '../images/map-marker.png';
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
+import { RectButton } from 'react-native-gesture-handler';
+import api from '../services/api';
+
+interface Library{
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
 
 export default function LibrariesMap(){
 
+    const [libraries, setLibraries] = useState<Library[]>([]); 
     const navigation = useNavigation();
-    function handleNavigateToLibraryDetails(){
-        navigation.navigate('LibraryDetails');
+
+    useFocusEffect(() => {
+      api.get('libraries').then(response => {
+        setLibraries(response.data);
+      });
+    });
+
+    function handleNavigateToLibraryDetails(id: number){
+        navigation.navigate('LibraryDetails', {id});
     }
+    function handleNavigateToCreateLibrary(){
+        navigation.navigate('SelectMapPosition');
+    }
+    
     return(
         <View style={styles.container}>
-      <MapView 
-      style={styles.map} 
-      provider={PROVIDER_GOOGLE}
-      initialRegion={{
-        latitude:-22.8931889,
-        longitude:-43.2464443,
-        latitudeDelta: 0.2,
-        longitudeDelta: 0.2,
-      }}>
-        <Marker 
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.7,
-            y: 0.8,
-          }}
-          coordinate={{
+          <MapView 
+          style={styles.map} 
+          provider={PROVIDER_GOOGLE}
+          initialRegion={{
             latitude:-22.8931889,
-            longitude: -43.2464443,
-          }}
-        >
-          <Callout tooltip={true} onPress={handleNavigateToLibraryDetails}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Biblioteca Parque Estadual</Text>
-            </View>
-          </Callout>
-        </Marker>
+            longitude:-43.2464443,
+            latitudeDelta: 0.2,
+            longitudeDelta: 0.2,
+          }}>
+
+          {libraries.map(library => {
+            return (
+              <Marker 
+                key={library.id}
+                icon={mapMarker}
+                calloutAnchor={{
+                  x: 2.7,
+                  y: 0.8,
+                }}
+                coordinate={{
+                  latitude:library.latitude,
+                  longitude: library.longitude,
+                }}
+              >
+                <Callout tooltip={true} onPress={() => handleNavigateToLibraryDetails(library.id)}>
+                  <View style={styles.calloutContainer}>
+                    <Text style={styles.calloutText}>{library.name}</Text>
+                  </View>
+                </Callout>
+              </Marker>
+            );
+          })}
       </MapView>
 
       <View style={styles.footer}>
-        <Text style={styles.footerText}>2 bibliotecas encontradas</Text>
-        <TouchableOpacity style={styles.createLibraryButton} onPress={() => {}}>
+        <Text style={styles.footerText}>{libraries.length} bibliotecas encontradas</Text>
+        <RectButton style={styles.createLibraryButton} onPress={handleNavigateToCreateLibrary}>
             <Feather name="plus" size={20} color="#FFF"/>
-        </TouchableOpacity>
+        </RectButton>
       </View>
     </View>
     );
--- mobile/src/routes.tsx
+++ mobile/src/routes.tsx
@@ -5,14 +5,39 @@ import { createStackNavigator } from '@react-navigation/stack';
 import LibrariesMap from './pages/LibrariesMap';
 import LibraryDetails from './pages/LibraryDetails';
 
+import SelectMapPosition from './pages/CreateLibrary/SelectMapPosition';
+import LibraryData from './pages/CreateLibrary/LibraryData';
+
+import Header from './components/Header';
+
+
 const {Navigator, Screen} = createStackNavigator();
 
 export default function Routes(){
     return (
         <NavigationContainer>
-            <Navigator screenOptions={{headerShown: false}}>
+            <Navigator screenOptions={{headerShown: false, cardStyle: {backgroundColor: '#f2f3f5'}}}>
+                
                 <Screen name="LibrariesMap" component={LibrariesMap} />
-                <Screen name="LibraryDetails" component={LibraryDetails} />
+                
+                <Screen name="LibraryDetails" 
+                    component={LibraryDetails} 
+                    options={{headerShown: true, 
+                    header: () => <Header showCancel={false} title="Biblioteca"/>
+                    }} 
+                />
+                <Screen name="SelectMapPosition" 
+                    component={SelectMapPosition} 
+                    options={{headerShown: true, 
+                        header: () => <Header title="Selecione no mapa"/>
+                    }} 
+                />
+                <Screen name="LibraryData" 
+                    component={LibraryData} 
+                    options={{headerShown: true, 
+                        header: () => <Header title="Informe os dados"/>
+                    }} 
+                />
             </Navigator>
         </NavigationContainer>
     );
